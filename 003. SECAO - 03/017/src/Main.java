3013ef81bac95624b6bb4905cb0234fc7197cdd4
Fabio-Pina <fabiopina422@gmail.com>
2022-01-08 23:16:01 -0300

    Curso de Java - Programação Orientada a Objetos

---
+++ 003. SECAO - 03/017/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b;
+
+        System.out.println(resultado);
+
+        sc.close();
+    }
+}
